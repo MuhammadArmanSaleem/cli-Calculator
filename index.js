2a851616657d947d0ccaa489adbe227ae15c5987
MuhammadArmanSaleem <armansaleem326@gmail.com>
2024-04-03 13:17:20 +0500

    cli simple calculator

---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node   
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your First Number", type: "number", name: "FirstNumber" },
+    { message: "Enter your Second Number", type: "number", name: "SecondNumber" },
+    {
+        message: "Select on of the Operator to perform Operations",
+        type: "list",
+        name: "Operator",
+        choices: [
+            "Addition",
+            "Multiplication",
+            "Subtraction",
+            "Division",
+            "Remainder/Modulus",
+        ],
+    },
+]);
+//Conditional Statment
+if (answer.Operator === "Addition") {
+    console.log("Your Value is :");
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+if (answer.Operator === "Subtraction") {
+    console.log("Your Value is :");
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+if (answer.Operator === "Multiplication") {
+    console.log("Your Value is :");
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+if (answer.Operator === "Division") {
+    console.log("Your Value is :");
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+if (answer.Operator === "Remainder/Modulus") {
+    console.log("Your Value is :");
+    console.log(answer.FirstNumber % answer.SecondNumber);
+}
